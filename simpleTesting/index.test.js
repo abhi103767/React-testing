57a868886bf85742418a4c5a4d690b55ff03b900
Avinash <avinash1234sharma1@gmail.com>
2022-05-25 11:18:29 +0530

    multiple testCases

--- simpleTesting/index.test.js
+++ simpleTesting/index.test.js
@@ -1,1 +1,13 @@
 const {test} = require('@jest/globals')
+const { expect } = require('expect')
+
+function sum(a,b){
+    return  a + b;
+}
+
+test("Should Work on positive Number", function(){
+    expect(sum(2,2)).toBe(4);
+    expect(sum(0,0)).toBe(0);
+    expect(sum(10,15)).toBe(25);
+    expect(sum(100,0)).toBe(100)
+})
