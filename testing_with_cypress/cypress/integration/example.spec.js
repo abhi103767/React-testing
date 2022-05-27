939739746b9cca66aad4c6aa40de99d2d99fc578
Avinash <avinash1234sharma1@gmail.com>
2022-05-27 10:58:24 +0530

    changes made

---
+++ testing_with_cypress/cypress/integration/example.spec.js
@@ -0,0 +1,9 @@
+
+describe('input form', () => {
+
+    it('should visit the page' , () => {
+        cy.visit('http://localhost:3000')
+       
+    })
+
+})
--- testing_with_cypress/src/App.js
+++ testing_with_cypress/src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count,setCount] = useState(0);
   return (
     <div className="App">
+    <p>{count}</p>
+    <button onClick={() => {
+      setCount(count+1)
+    }}>add</button>
+     
+
       
     </div>
   );
